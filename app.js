e01496f9f6461d3cd8b645f9c6af1451c4a3fa8f
Daniel Hawn <danieljhawn@gmail.com>
2021-03-02 14:38:11 -0600

    trying to fix route

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require("express");
 const path = require('path');
 const app = express();
 const route = express.Router();
-const port = 5000;
+const port = 5500 || process.env.PORT;
 
 // View engine setup
 app.engine('handlebars', exphbs({
