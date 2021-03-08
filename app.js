4ea737cf3847d872209c4a94bc52f0f6df19bb47
Daniel Hawn <danieljhawn@gmail.com>
2021-03-08 14:59:02 -0600

    changing the port

--- app.js
+++ app.js
@@ -65,8 +65,8 @@ app.post('/send-email', function (req, res) {
       // create reusable transporter object using the default SMTP transport
       let transporter = nodemailer.createTransport({
         host: process.env.SMTP_SERVER,
-        port: 465,
-        secure: true, // true for 465, false for other ports
+        port: 587,
+        secure: false, // true for 465, false for other ports
         auth: {
           user: process.env.SMTP_LOGIN, // generated ethereal user
           pass: process.env.SMTP_PASSWORD, // generated ethereal password
